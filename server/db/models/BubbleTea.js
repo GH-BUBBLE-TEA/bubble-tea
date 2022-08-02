86aec1f41970102d8f71c7bbc0f7494fe30d2043
Yuri Yoo <yuriyoo@hotmail.com>
2022-08-02 17:52:54 -0400

    created db models

---
+++ server/db/models/BubbleTea.js
@@ -0,0 +1,37 @@
+const Sequelize = require("sequelize");
+const db = require("../db");
+
+const BubbleTea = db.define("bubbletea", {
+  teaName: {
+    type: Sequelize.STRING,
+    unique: true,
+    allowNull: false,
+  },
+  teaCategories: {
+    type: Sequelize.STRING,
+    status: {
+      type: Sequelize.ENUM("milk tea", "fruit tea", "latte"),
+    },
+    allowNull: false,
+  },
+  imageURL: {
+    type: Sequelize.TEXT,
+    defaultValue:
+      "https://d3q0fpse3wbo5h.cloudfront.net/production/uploads/innovations/kung_fu_bubble_tea.jpg",
+  },
+  defaultPrice: {
+    type: Sequelize.DECIMAL,
+    defaultValue: 10,
+  },
+  description: {
+    type: Sequelize.TEXT,
+  },
+  stock: {
+    type: Sequelize.INTEGER,
+    validate: {
+      min: 0,
+    },
+  },
+});
+
+module.exports = BubbleTea;
