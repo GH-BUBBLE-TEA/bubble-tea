7701444b69ca1e1f4c5040d903853ed64a1ebe4d
Wei liu <weihuiliu1228sh@gmail.com>
2022-08-03 22:13:54 -0400

    adding shopping cart page

--- client/components/Menu.js
+++ client/components/Menu.js
@@ -4,27 +4,66 @@ import { Link } from "react-router-dom";
 import { getBubbleTeas } from "../store/bubbleTeas";
 
 export class Menu extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      teaFilter: "All",
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
   componentDidMount() {
     this.props.fetchData();
   }
 
+  handleChange(event) {
+    this.setState({ teaFilter: event.target.value });
+  }
+
   render() {
+    const allbubbleTeas = this.props.bubbleTeas;
+    console.log(this.props);
+    allbubbleTeas.filter((singleBubbleTea) => {
+      if (this.state.teaFilter === "Milk Tea") {
+        return singleBubbleTea.teaCategories === "Milk Tea";
+      } else if (this.state.teaFilter === "Fruit Tea") {
+        return singleBubbleTea.teaCategories === "Fruit Tea";
+      } else if (this.state.teaFilter === "Latte") {
+        return singleBubbleTea.teaCategories === "Latte";
+      } else {
+        return singleBubbleTea;
+      }
+    });
     return (
       <div>
         <h1>MENU</h1>
-        <Link to="/cart">Cart</Link>
+        <Link to="/cart">Cart </Link>
         <div>
-          {this.props.bubbleTeas.map((bubbleTea) => (
-            <div key={bubbleTea.id}>
-              <div>
-                <Link to={`/menu/${bubbleTea.id}`}>
-                  <img src={bubbleTea.imageURL} />
-                  <h3>{bubbleTea.teaName}</h3>
-                </Link>
-                <p>Price: ${bubbleTea.defaultPrice}</p>
+          <select
+            id="category"
+            value={this.state.teaFilter}
+            onChange={this.handleChange}
+          >
+            <option value="All">All</option>
+            <option value="Milk Tea">Milk Tea</option>
+            <option value="Fruit Tea">Fruit Tea</option>
+            <option value="Latte">Latte</option>
+          </select>
+        </div>
+
+        <div className="bubble-tea-list">
+          {allbubbleTeas.map((bubbleTea) => {
+            return (
+              <div key={bubbleTea.id}>
+                <div>
+                  <Link to={`/menu/${bubbleTea.id}`}>
+                    <img src={bubbleTea.imageURL} />
+                    <h3>{bubbleTea.teaName}</h3>
+                  </Link>
+                  <p>Price: ${bubbleTea.defaultPrice}</p>
+                </div>
               </div>
-            </div>
-          ))}
+            );
+          })}
         </div>
       </div>
     );
